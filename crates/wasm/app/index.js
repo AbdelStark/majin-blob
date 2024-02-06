c7c39c5332828c28bf9c9a2631ebe1bce76dec82
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2024-02-06 12:47:11 +0100

    💚 publish majin-blob-wasm npm package

--- crates/wasm/app/index.js
+++ crates/wasm/app/index.js
@@ -1,6 +1,8 @@
 import * as wasm from "majin-blob-wasm";
 
-document.getElementById("blobForm").addEventListener("submit", async function(event) {
+document
+  .getElementById("blobForm")
+  .addEventListener("submit", async function (event) {
     event.preventDefault(); // Prevent the form from submitting in the traditional way
 
     const fileInput = document.getElementById("blobFileInput");
@@ -8,17 +10,22 @@ document.getElementById("blobForm").addEventListener("submit", async function(ev
     let blob;
 
     if (fileInput.files.length > 0) {
-        // Read the blob from the file
-        const file = fileInput.files[0];
-        blob = await file.text();
+      // Read the blob from the file
+      const file = fileInput.files[0];
+      blob = await file.text();
     } else {
-        // Use the manually entered blob data
-        blob = textInput.value;
+      // Use the manually entered blob data
+      blob = textInput.value;
     }
     const state_diffs = wasm.blob_recover(blob);
 
     // Format the JSON string to be pretty
-    const formattedStateDiffs = JSON.stringify(JSON.parse(state_diffs), null, 2);
+    const formattedStateDiffs = JSON.stringify(
+      JSON.parse(state_diffs),
+      null,
+      2,
+    );
 
-    document.getElementById("stateDiffsOutput").textContent = formattedStateDiffs;
-});
+    document.getElementById("stateDiffsOutput").textContent =
+      formattedStateDiffs;
+  });
