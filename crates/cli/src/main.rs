25aaa2ae6b14c2969b3035d2554c8945e95428a9
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2024-02-06 13:06:27 +0100

    🗑️ use if let for cli command

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -24,17 +24,12 @@ enum Commands {
 fn main() {
     let cli = Cli::parse();
 
-    match cli.command {
-        Some(Commands::Recover { blob_file }) => {
-            let blob_file = blob_file.unwrap();
-            let blob_data = serde::parse_file_to_blob_data(blob_file.to_str().unwrap());
-            let original_data = blob::recover(blob_data);
-            let state_diffs = serde::parse_state_diffs(original_data.as_slice());
-            let state_diffs_json = serde::to_json(state_diffs.as_slice());
-            println!("{}", state_diffs_json);
-        }
-        None => {
-            println!("No command provided");
-        }
+    if let Some(Commands::Recover { blob_file }) = cli.command {
+        let blob_file = blob_file.unwrap();
+        let blob_data = serde::parse_file_to_blob_data(blob_file.to_str().unwrap());
+        let original_data = blob::recover(blob_data);
+        let state_diffs = serde::parse_state_diffs(original_data.as_slice());
+        let state_diffs_json = serde::to_json(state_diffs.as_slice());
+        println!("{}", state_diffs_json);
     }
 }
