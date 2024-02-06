10d13dbbade1248d9263839af4d5300c17856f0c
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2024-02-06 11:27:23 +0100

    ✨ wasm

---
+++ crates/wasm/src/lib.rs
@@ -0,0 +1,19 @@
+mod utils;
+
+use majin_blob_core::blob::recover;
+use majin_blob_types::serde::{parse_state_diffs, parse_str_to_blob_data, to_json};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn blob_recover(data: &str) -> String {
+    let blob_data = parse_str_to_blob_data(data);
+    let original_data = recover(blob_data);
+    let state_diffs = parse_state_diffs(&original_data);
+    let state_diffs_json = to_json(state_diffs.as_slice());
+    state_diffs_json
+}
